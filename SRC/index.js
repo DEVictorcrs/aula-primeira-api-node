fc42e25c919bafeb52e27faa23a0617a16ba3c69
DEVictorcrs <victorcostars@gmail.com>
2025-06-01 19:31:18 -0300

    atualização das tabelas dos bancos de dados

--- SRC/index.js
+++ SRC/index.js
@@ -25,12 +25,33 @@ app.get('/cliente', async (request, response) => {
 
 });
 
+app.get('/endereco', async (request, response) => {
+
+    try {
+        
+        const result = await pool.query('SELECT * FROM endereco');
+
+        response.send(result.rows); 
+
+    }catch(err){
+        response.status(500).send(err)
+    }
+
+});
+
+
 app.listen(PORT, ()=>{
 
     console.log("Servidor está rodando na porta " + 3000);
 
 });
 
+app.get('/detalhecliente', async (request,response)=>{
 
-//https://localhost:3000/helloworld
+    const result = await pool.query('SELECT * FROM cliente cl join endereco dc on (id_cliente = id_tipo_cliente)');
+
+    response.send(result.rows);
+})
+
+//http://localhost:3000/helloworld
 
